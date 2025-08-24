eb1b0449c635ef7082a74153fbe70724df43c006
chard <dev.richardpesino@gmail.com>
2025-08-25 02:42:53 +0800

    initial commit

---
+++ src/main/java/com/chrd/myecomm/db/service/MyecommDbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.chrd.myecomm.db.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyecommDbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyecommDbServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chrd/myecomm/db/service/MyecommDbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrd.myecomm.db.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyecommDbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
